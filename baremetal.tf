ac272da2a7ae5ed52d28c321c80fd1645da5e3f8
jvallexm <jvallexm@gmail.com>
2021-05-04 11:45:01 -0400

    First

---
+++ baremetal.tf
@@ -0,0 +1,22 @@
+##############################################################################
+# Create Baremetal Server
+##############################################################################
+
+resource ibm_compute_bare_metal baremetal {
+    hostname             = var.hostname
+    domain               = var.domain
+    os_reference_code    = "UBUNTU_16_64"
+    datacenter           = "sjc04"
+    hourly_billing       = true 
+    private_network_only = true
+    fixed_config_preset  = var.fixed_config_preset
+    private_vlan_id      = var.private_vlan_id
+
+    user_metadata = "{\"value\":\"newvalue\"}" # Optional
+    tags = [
+      "delete-me"
+    ]
+    notes = "note test"
+}
+
+##############################################################################s
---
+++ variables.tf
@@ -0,0 +1,56 @@
+##############################################################################
+# Account variables
+##############################################################################
+
+variable TF_VERSION {
+ default     = "0.13"
+ description = "The version of the Terraform engine that's used in the Schematics workspace."
+}
+
+variable iaas_classic_username {
+  description = "IBM Cloud classic username"
+  type        = string
+}
+
+variable iaas_classic_api_key {
+  description = "IBM Cloud classic api key"
+  type        = string
+}
+
+variable resource_group {
+    description = "Name of resource group where all infrastructure will be provisioned"
+    type        = string
+    default     = "asset-development"
+
+    validation  {
+      error_message = "Unique ID must begin and end with a letter and contain only letters, numbers, and - characters."
+      condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.resource_group))
+    }
+}
+
+##############################################################################
+# Baremetal Variables
+##############################################################################
+
+variable hostname {
+  description = "Name of host for virtual server"
+  type        = string
+  default     = "example-baremetal"
+}
+
+variable domain {
+  description = "Domain name for baremetal server"
+  type        = string
+  default     = "example.com"
+}
+
+variable fixed_config_preset  {
+  description = "Config preset for baremetal server"
+  type        = string
+}
+
+variable private_vlan_id {
+  description = "Private VLAN ID"
+}
+
+##############################################################################
