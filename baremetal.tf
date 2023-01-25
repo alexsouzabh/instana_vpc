d2bda0cab292c75c83ab1881ebc883ebf09a3b83
Alexandre Souza <alexandresouza@Alexandres-MacBook-Pro.local>
2023-01-25 18:20:04 -0300

    add vlan e VM

--- baremetal.tf
+++ baremetal.tf
@@ -2,22 +2,60 @@
 # Create Baremetal Server
 ##############################################################################
 
-resource ibm_compute_bare_metal baremetal {
-    hostname             = var.hostname
-    domain               = var.domain
-    os_reference_code    = var.SO
-    datacenter           = var.DC
-    hourly_billing       = true 
-    private_network_only = true
-    fixed_config_preset  = var.fixed_config_preset
-    //private_vlan_id      = var.private_vlan_id
-
-    user_metadata = "{\"value\":\"newvalue\"}" # Optional
-    tags = [
-      "delete-me"
-    ]
-    notes = "note test"
+resource "ibm_network_vlan" "test_vlan_public" {
+  name            = "public_vlan"
+  datacenter      = var.SO
+  type            = "PUBLIC"
+  tags = [
+    "collectd",
+    "mesos-master",
+  ]
 }
+
+resource "ibm_network_vlan" "test_vlan_private" {
+  name            = "private_vlan"
+  datacenter      = var.SO
+  type            = "PRIVATE"
+  tags = [
+    "collectd",
+    "mesos-master",
+  ]
+}
+
+resource "ibm_compute_vm_instance" "twc_terraform_sample" {
+  hostname                   = var.hostname
+  domain                     = var.domain
+  os_reference_code          = var.SO
+  datacenter                 = var.DC
+  network_speed              = 100
+  hourly_billing             = true
+  private_network_only       = false
+  cores                      = 1
+  memory                     = 1024
+  disks                      = [25, 10, 20]
+  user_metadata              = "{\"value\":\"newvalue\"}"
+  dedicated_acct_host_only   = true
+  local_disk                 = false
+  public_vlan_id             = ibm_network_vlan.test_vlan_public.ID
+  private_vlan_id            = ibm_network_vlan.test_vlan_private.ID
+}
+
+#resource ibm_compute_bare_metal baremetal {
+#    hostname             = var.hostname
+#    domain               = var.domain
+#    os_reference_code    = var.SO
+#    datacenter           = var.DC
+#    hourly_billing       = true 
+#    private_network_only = true
+#    fixed_config_preset  = var.fixed_config_preset
+#    //private_vlan_id      = var.private_vlan_id
+#
+#    user_metadata = "{\"value\":\"newvalue\"}" # Optional
+#    tags = [
+#      "delete-me"
+#    ]
+#    notes = "note test"
+#}
 
 
 ##############################################################################s
