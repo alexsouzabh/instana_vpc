796cc4573fb3eb973a3d9c8f7095d9ccc2beec0f
Alexandre Souza <alexandresouza@Alexandres-MacBook-Pro.local>
2023-01-26 17:03:04 -0300

    Creating multiple servers

--- baremetal.tf
+++ baremetal.tf
@@ -24,7 +24,7 @@ resource "ibm_network_vlan" "test_vlan_private" {
 
 resource "ibm_compute_vm_instance" "twc_terraform_sample" {
   count                      = var.server_qtde
-  #hostname                   = var.hostname
+  hostname                   = "Server-0$(count.index + 1)"
   domain                     = var.domain
   os_reference_code          = var.SO
   datacenter                 = var.DC
@@ -43,9 +43,7 @@ resource "ibm_compute_vm_instance" "twc_terraform_sample" {
     ibm_network_vlan.test_vlan_private,
     ibm_network_vlan.test_vlan_public
   ]
-  tags = {
-    hostname = "Server-0$(count.index + 1)"
-  }
+  
 }
 
 #resource ibm_compute_bare_metal baremetal {
