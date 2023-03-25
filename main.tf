d6f3b3e12d6fd9069a13735e4815e283b25a4416
Alexandre Souza <alexandresouza@Alexandres-MacBook-Pro.local>
2023-03-25 19:10:26 -0300

    update

--- main.tf
+++ main.tf
@@ -63,6 +63,21 @@ data "ibm_is_image" "centos" {
     name = "ibm-centos-7-6-minimal-amd64-1"
 }
 
+resource "ibm_is_instance" "vsi-monitor" {
+    count   = var.monitoring_qtde
+    name    = "${var.monitoring_basename}-${count.index + 1}"
+    vpc     = ibm_is_vpc.vpc-instance.id
+    zone    = var.vpc-zones[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))]
+    keys    = [ibm_is_ssh_key.ssh_key_id.id]
+    image   = data.ibm_is_image.centos.id
+    profile = var.monitoring_profile
+
+    primary_network_interface {
+        subnet          = ibm_is_subnet.subnet[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))].id
+        security_groups = [ibm_is_security_group.sg1.id]
+    }
+}
+
 resource "ibm_is_instance" "vsi-cassandra" {
     count   = var.cassandra_qtde
     name    = "${var.cassandra_basename}-${count.index + 1}"
@@ -86,23 +101,6 @@ resource "ibm_is_instance" "vsi-clickhouse" {
     keys    = [ibm_is_ssh_key.ssh_key_id.id]
     image   = data.ibm_is_image.centos.id
     profile = var.clickhouse_profile
-
-    primary_network_interface {
-        subnet          = ibm_is_subnet.subnet[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))].id
-        security_groups = [ibm_is_security_group.sg1.id]
-    }
-}
-
-
-
-resource "ibm_is_instance" "vsi-monitor" {
-    count   = var.monitoring_qtde
-    name    = "${var.monitoring_basename}-${count.index + 1}"
-    vpc     = ibm_is_vpc.vpc-instance.id
-    zone    = var.vpc-zones[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))]
-    keys    = [ibm_is_ssh_key.ssh_key_id.id]
-    image   = data.ibm_is_image.centos.id
-    profile = var.monitoring_profile
 
     primary_network_interface {
         subnet          = ibm_is_subnet.subnet[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))].id
