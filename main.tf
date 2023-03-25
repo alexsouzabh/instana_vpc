c8cb65bfbbcd82667f7700126f641092f4502f6c
Alexandre Souza <alexandresouza@Alexandres-MacBook-Pro.local>
2023-03-25 19:14:12 -0300

    update

--- main.tf
+++ main.tf
@@ -63,14 +63,14 @@ data "ibm_is_image" "centos" {
     name = "ibm-centos-7-6-minimal-amd64-1"
 }
 
-resource "ibm_is_instance" "vsi-monitor" {
-    count   = var.monitoring_qtde
-    name    = "${var.monitoring_basename}-${count.index + 1}"
+resource "ibm_is_instance" "vsi-abc" {
+    count   = var.cassandra_qtde
+    name    = "${var.cassandra_basename}-${count.index + 10}"
     vpc     = ibm_is_vpc.vpc-instance.id
     zone    = var.vpc-zones[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))]
     keys    = [ibm_is_ssh_key.ssh_key_id.id]
     image   = data.ibm_is_image.centos.id
-    profile = var.monitoring_profile
+    profile = var.cassandra_profile
 
     primary_network_interface {
         subnet          = ibm_is_subnet.subnet[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))].id
