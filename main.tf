15d55f05c61b1a36f44514b95e67d17866121f4c
Alexandre Souza <alexandresouza@Alexandres-MacBook-Pro.local>
2023-03-26 16:56:23 -0300

    update

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "ibm_is_instance" "vsi-monitoring" {
     zone    = var.vpc-zones[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))]
     keys    = [ibm_is_ssh_key.ssh_key_id.id]
     image   = data.ibm_is_image.centos.id
-    profile = var.cassandra_profile
+    profile = var.monitoring_profile
 
     primary_network_interface {
         subnet          = ibm_is_subnet.subnet[count.index - (floor(count.index/length(var.vpc-zones))*length(var.vpc-zones))].id
